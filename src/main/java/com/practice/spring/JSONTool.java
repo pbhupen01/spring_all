845cc6c064bbc0482e1ae0a2ecfc02c9f424ebdd
Bhupendra Pandey <pandeybhupen01@gmail.com>
2018-02-07 12:23:42 +0530

    Added tool to convert object to JSON

---
+++ src/main/java/com/practice/spring/JSONTool.java
@@ -0,0 +1,20 @@
+package com.practice.spring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.spring.dto.UserRequest;
+
+import java.io.IOException;
+
+public class JSONTool {
+
+    public static void main(String[] a){
+        UserRequest object = new UserRequest();
+        ObjectMapper mapperObj = new ObjectMapper();
+        try {
+            String jsonStr = mapperObj.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+            System.out.println(jsonStr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
