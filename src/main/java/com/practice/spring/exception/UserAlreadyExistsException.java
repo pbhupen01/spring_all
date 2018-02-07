7bab2fa8ac7a67525420fab70e75b220ca3b04fc
pbhupen01 <pandeybhupen01@gmail.com>
2018-02-08 00:04:58 +0530

    Few gold plating

--- src/main/java/com/practice/spring/exception/UserAlreadyExistsException.java
+++ src/main/java/com/practice/spring/exception/UserAlreadyExistsException.java
@@ -2,8 +2,15 @@ package com.practice.spring.exception;
 
 public class UserAlreadyExistsException extends Exception {
 
+    private String userId;
+
     public UserAlreadyExistsException(String userId)
     {
         super(String.format("User with ID '%s' already exists", userId));
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 }
--- src/main/java/com/practice/spring/exception/UserNotFoundException.java
+++ src/main/java/com/practice/spring/exception/UserNotFoundException.java
@@ -2,8 +2,15 @@ package com.practice.spring.exception;
 
 public class UserNotFoundException extends Exception {
 
+    String userId;
+
     public UserNotFoundException(String userId)
     {
         super(String.format("User with ID '%s' not found", userId));
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 }
