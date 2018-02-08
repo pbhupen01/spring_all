c71e94d9cfadd38bafeca52f8ce1b0b3211cbdb9
Bhupendra Pandey <pandeybhupen01@gmail.com>
2018-02-08 11:54:48 +0530

    Enabled NoHandlerFoundException Added more error handling in RestErrorHandlerController

--- src/main/java/com/practice/spring/handler/RestErrorHandlerController.java
+++ src/main/java/com/practice/spring/handler/RestErrorHandlerController.java
@@ -8,18 +8,34 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 @ControllerAdvice
 @Slf4j
 public class RestErrorHandlerController extends ResponseEntityExceptionHandler {
 
     private static String ERROR_MESSAGE = "Error occurred while processing request. Sending Error: ";
+
+    /*@Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+                                                             Object body,
+                                                             HttpHeaders headers,
+                                                             HttpStatus status,
+                                                             WebRequest request)
+    {
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,"Could not read JSON document.", ex);
+        log.error(ERROR_MESSAGE + errorResponse, ex);
+        return buildResponseEntity(errorResponse);
+    }*/
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
@@ -52,6 +68,17 @@ public class RestErrorHandlerController extends ResponseEntityExceptionHandler {
         return buildResponseEntity(errorResponse);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                         HttpHeaders headers,
+                                                                         HttpStatus status,
+                                                                         WebRequest request)
+    {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage(), ex);
+        log.error(ERROR_MESSAGE + errorResponse, ex);
+        return buildResponseEntity(errorResponse);
+    }
+
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleException(UserNotFoundException ex) {
@@ -63,6 +90,13 @@ public class RestErrorHandlerController extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Object> handleException(UserAlreadyExistsException ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+        log.error(ERROR_MESSAGE + errorResponse, ex);
+        return buildResponseEntity(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleException(MethodArgumentTypeMismatchException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
         log.error(ERROR_MESSAGE + errorResponse, ex);
         return buildResponseEntity(errorResponse);
     }
