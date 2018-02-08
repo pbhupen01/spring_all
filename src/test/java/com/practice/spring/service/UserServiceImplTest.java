8a44ae93b267157855706be4ecfd5c53e87cb933
Bhupendra Pandey <pandeybhupen01@gmail.com>
2018-02-08 18:14:02 +0530

    Continuation to previous checkin - Adding Test cases

--- src/test/java/com/practice/spring/service/UserServiceImplTest.java
+++ src/test/java/com/practice/spring/service/UserServiceImplTest.java
@@ -8,16 +8,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest(classes = UserServiceImpl.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class UserServiceImplTest {
 
     //    @Value("${mano.microservices.runtime-catalog.url}")
