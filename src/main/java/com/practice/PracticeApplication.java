0e2bf883e93489e876761158c03bba2b55fa3797
pbhupen01 <pandeybhupen01@gmail.com>
2018-02-12 22:52:33 +0530

    Added commented annotations for Spring caching

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -2,8 +2,11 @@ package com.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+// Add this to enable spring caching
+//@EnableCaching
 public class PracticeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/practice/spring/repository/UserRepository.java
+++ src/main/java/com/practice/spring/repository/UserRepository.java
@@ -4,8 +4,13 @@ import com.practice.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Cacheable;
+
 // @RepositoryRestResource can be used to directly expose end points for this repository
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    //@Cacheable("books")
+    // add this to method to make it cacheable
 
 }
